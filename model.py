bdf34d55b9f87b85519c0aac36af41c8f3b99c47
nickofca <nickofca@email.arizona.edu>
2018-11-03 00:03:16 -0700

    completion of heat map features

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ import pandas
 from sklearn import model_selection, linear_model
 import numpy
 import seaborn 
+import matplotlib.pyplot as plt
 
 inputFile = inpF = "data/Nick file.txt"
 
@@ -25,12 +26,13 @@ def modeler(inp):
     train, test = model_selection.train_test_split(inpD, random_state = randomSeed)
     
     ##Predict outcomes
-    model = linear_model.LogisticRegression(random_state=randomSeed).fit(test[["SeptBees","SeptMites"]],test["Survived"])
-    testPrediction = model.predict_proba(train[["SeptBees","SeptMites"]])
+    model = linear_model.LogisticRegression(random_state=randomSeed).fit(train[["SeptBees","SeptMites"]],train["Survived"])
+    testPrediction = model.predict_proba(test[["SeptBees","SeptMites"]])
     testPredRound = (testPrediction[:,1] > probabilityThreshold)
-    testDif = testPredRound == train["Survived"]
-    testAccuracy = testDif.sum()/train.shape[0]
-    
+    testDif = testPredRound == test["Survived"]
+    testAccuracy = testDif.sum()/test.shape[0]
+    testPrecision = (testPredRound == test["Survived"]).sum()/testPredRound.sum()
+    testRecall = (testPredRound == test["Survived"]).sum()/test["Survived"].sum()
     
     ##Do a grid run with the model
     xmin = int(numpy.floor(inpD["SeptBees"].min()))
@@ -40,11 +42,19 @@ def modeler(inp):
     for x in range(xmin,xmax):
         for y in miteAxis:
             #numpy.asscalar to turn numpy float64 into native python float
-            hmD.loc[y,x] = numpy.asscalar(model.predict_proba([[x,y]])[0][1])
-    
+            hmD.loc[y,x] = model.predict_proba([[x,y]])[0][1]
+        #Inner elements are stored as objects; converted to numpy.float64
+        hmD[x]=pandas.to_numeric(hmD[x])
+        
     ##Create heat map
-    seaborn.heatmap(hmD.values)
+    fig, ax = plt.subplots(figsize=(15,15)) 
+    seaborn.heatmap(hmD.T,vmin=0,vmax=1,cbar_kws={"shrink": 0.35},linewidths = .6, square=True, ax=ax, annot=True)
+    plt.xlabel("Mites per 100 bees")
+    plt.ylabel("Frames of Bees")
+    plt.tight_layout()
+    plt.savefig("heatmap.png")
+    plt.show()
     
-    return(model,testAccuracy)
+    return(model,testAccuracy, testPrecision, testRecall)
 
-model, testAccuracy = modeler(inpF)
+model, testAccuracy, testPrecision, testRecall = modeler(inpF)
